2849db24932ae2c2e550756626fe27d92914ef57
Adrian Fernandes <boybothere@gmail.com>
2025-09-03 22:27:28 +0530

    feat(login): add input state handling & loading spinner

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,38 +1,65 @@
 import React from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
+import { login, signup } from '../../firebase'
+import netflix_spinner from '../../assets/netflix_spinner.gif'
 
 const Login = () => {
 
     const [signState, setSignState] = React.useState('Sign In')
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [loading, setLoading] = React.useState(false);
+
+    const resetFields = () => {
+        setName("");
+        setEmail("");
+        setPassword("");
+    };
+
+
+    const user_auth = async (event) => {
+        event.preventDefault();
+        setLoading(true);
+        if (signState === "Sign In") {
+            await login(email, password);
+        } else {
+            await signup(name, email, password);
+        }
+        setLoading(false);
+    }
 
     return (
-        <div className='login'>
-            <img src={logo} className='login-logo' alt="" />
-            <div className="login-form">
-                <h1>{signState}</h1>
-                <form>
-                    {signState === "Sign Up" ? <input type="text" placeholder='Your name' /> : <></>}
-                    <input type="email" placeholder='Email' />
-                    <input type="password" placeholder='Password' />
-                    <button>{signState}</button>
+        loading ? <div className="login-spinner">
+            <img src={netflix_spinner} alt="" />
+        </div> :
+            <div className='login'>
+                <img src={logo} className='login-logo' alt="" />
+                <div className="login-form">
+                    <h1>{signState}</h1>
+                    <form>
+                        {signState === "Sign Up" ? <input value={name} onChange={(e) => { setName(e.target.value) }} type="text" placeholder='Your name' /> : <></>}
+                        <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder='Email' />
+                        <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder='Password' />
+                        <button onClick={user_auth} type='submit'>{signState}</button>
 
-                    <div className="form-help">
-                        <div className="remember">
-                            <input type="checkbox" />
-                            <label htmlFor="">Remember Me</label>
+                        <div className="form-help">
+                            <div className="remember">
+                                <input type="checkbox" />
+                                <label htmlFor="">Remember Me</label>
+                            </div>
+                            <p>Need Help?</p>
                         </div>
-                        <p>Need Help?</p>
-                    </div>
-                </form>
-                <div className="form-switch">
-                    {signState === "Sign In" ?
-                        <p>New to Netflix?  <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span></p> :
-                        <p>Already have an account?  <span onClick={() => setSignState("Sign In")}>Sign In Now</span></p>}
+                    </form>
+                    <div className="form-switch">
+                        {signState === "Sign In" ?
+                            <p>New to Netflix?  <span onClick={() => { setSignState("Sign Up"); resetFields(); }}>Sign Up Now</span></p> :
+                            <p>Already have an account?  <span onClick={() => { setSignState("Sign In"); resetFields(); }}>Sign In Now</span></p>}
 
+                    </div>
                 </div>
             </div>
-        </div>
     )
 }
 
