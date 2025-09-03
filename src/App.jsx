0034b3944be43377b785cb27075e6666efa44191
Adrian Fernandes <boybothere@gmail.com>
2025-09-03 22:29:22 +0530

    feat(app): add global toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home/Home'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
+import { Slide, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log("Logged in");
+        navigate('/');
+      } else {
+        console.log("Logged out");
+        navigate('/login');
+      }
+    });
+  }, [])
   return (
     <div>
+      <ToastContainer theme="dark" transition={Slide} />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
